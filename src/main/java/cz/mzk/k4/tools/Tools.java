e60126d801c8fab8191e8ba27fdf0480eff8fa56
MartinRumanek <martin@rumanek.cz>
2013-08-15 09:38:30 +0200

    There is no need to be web application. Changed to Jar.

---
+++ src/main/java/cz/mzk/k4/tools/Tools.java
@@ -0,0 +1,31 @@
+package cz.mzk.k4.tools;
+
+import cz.mzk.k4.tools.servlets.Search;
+
+/**
+ * @author: Martin Rumanek
+ * @version: 30.7.13
+ */
+public class Tools {
+    public static void main(String[] args) {
+        if (args.length < 1 || args[0] == null) {
+            printUsage();
+            return;
+        }
+
+        if (args[0].equals("checklogs")) {
+            //CheckLogs.run();
+        } else if (args[0].equals("search")) {
+            Search.run();
+        }
+
+    }
+
+    private static void printUsage() {
+        System.out.println("Mozne parametry:");
+        //System.out.println("checkLogs");
+        System.out.println("search");
+
+    }
+
+}
---
+++ src/main/java/cz/mzk/k4/tools/servlets/Search.java
@@ -0,0 +1,35 @@
+package cz.mzk.k4.tools.servlets;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+
+import javax.ws.rs.core.MediaType;
+
+
+/**
+ * @author holmanj
+ *         Vypíše uuid svazků s neznámou dostupností
+ */
+public class Search {
+
+
+    public static void run() {
+        Client client = Client.create();
+
+        for (int offset = 0; offset < 1081; offset = offset + 20) {
+            WebResource resourse = client
+                    .resource("http://krameriusndktest.mzk.cz/search/r.jsp?fq=dostupnost:%22%22&offset="
+                            + offset);
+            String html = resourse.accept(MediaType.APPLICATION_XML).get(
+                    String.class);
+
+            String[] uuid_lines = html.split("id=\"res_monograph_uuid:");
+
+            for (int i = 1; i < uuid_lines.length; i++) {
+                String[] uuids = uuid_lines[i].split("\"");
+                System.out.println(uuids[0]);
+            }
+        }
+
+    }
+}
