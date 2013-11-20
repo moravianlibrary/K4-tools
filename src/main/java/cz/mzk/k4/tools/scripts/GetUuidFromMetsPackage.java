a0e66de1bf2f141d479a937cb14ae6a9b5c0dccf
Daniel Secik <dsecik@gmail.com>
2013-11-20 10:30:30 +0100

    priprava na ziskavani uuid z mets baliku
    Former-commit-id: b267a582f2a9949a35d009de43d9b20273ba2445

---
+++ src/main/java/cz/mzk/k4/tools/scripts/GetUuidFromMetsPackage.java
@@ -0,0 +1,51 @@
+package cz.mzk.k4.tools.scripts;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
+
+import cz.mzk.k4.tools.utils.Script;
+
+public class GetUuidFromMetsPackage implements Script {
+
+	
+	@Override
+	public void run(List<String> args) {
+		String path = args.get(0);
+        Iterator<File> iterator =  FileUtils.iterateFiles(new File(path), new SuffixFileFilter(".xml"), TrueFileFilter.INSTANCE);
+        
+        while(iterator.hasNext()) {
+            File file = iterator.next();
+            getUuidFromXml(file);
+        }
+		
+	}
+	
+	protected String getUuidFromXml(File file){
+		SAXReader reader = new SAXReader();
+		try{
+			Document document = reader.read(file);
+			
+			List listNodes = document.selectNodes( "//PageImage/@href" );
+			System.out.println(listNodes);
+			
+		}catch(DocumentException e){
+			e.printStackTrace();
+		}
+		return "t";
+	}
+
+	@Override
+	public String getUsage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
