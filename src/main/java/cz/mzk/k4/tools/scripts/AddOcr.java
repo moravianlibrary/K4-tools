6319cac48975c18314500be444d99a02d7988746
Jan Holman <jan.holman@mzk.cz>
2013-12-13 13:19:15 +0100

    merge
    Former-commit-id: 87939f7759e85a7e37640aaca94f591a1eb23d5c

--- src/main/java/cz/mzk/k4/tools/scripts/AddOcr.java
+++ src/main/java/cz/mzk/k4/tools/scripts/AddOcr.java
@@ -1,6 +1,5 @@
 package cz.mzk.k4.tools.scripts;
 
-import cz.mzk.k4.tools.providers.ChildrenUuid;
 import cz.mzk.k4.tools.utils.AccessProvider;
 import cz.mzk.k4.tools.utils.Script;
 import cz.mzk.k4.tools.utils.domain.DigitalObjectModel;
@@ -24,18 +23,13 @@ public class AddOcr implements Script {
     public void run(List<String> args) {
         UuidWorker addOcr = new AddOcrWorker(true);
         String uuid = args.get(0);
-<<<<<<< HEAD
+
         List<String> list;
         list = fedoraUtils.getChildrenUuids(uuid, DigitalObjectModel.PAGE);
 
         for (String uuidChild : list) {
             addOcr.run(uuidChild);
         }
-=======
-
-        fedoraUtils.applyToAllUuid(new ChildrenUuid(uuid, DigitalObjectModel.PAGE), addOcr);
-
->>>>>>> fd6219304e4e76f042194f80b770cd7fcad33608
     }
 
     @Override
