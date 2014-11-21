f871e5a281341cd914406fd467f728100af40d4b
Martin Rumanek <martin@rumanek.cz>
2014-11-21 12:25:56 +0100

    Generate Dublincore for vc

--- external/collectionIdentifiers.py
+++ external/collectionIdentifiers.py
@@ -2,14 +2,14 @@
 import urllib2
 import json
 
-api_vc = 'http://krameriusndktest.mzk.cz/search/api/v5.0/search?q=collection%3Avc\%3A54940333-8f1d-4d24-ba3b-ac042a38a949&fq=document_type:monograph&start=0&rows=500'
+api_vc = 'http://docker.mzk.cz/search/api/v5.0/search?q=collection%3Avc\%3A9784fefa-4ba8-4d6d-a4fc-85174d12aa23&fq=document_type:periodicalvolume&start=0&rows=500'
 req= urllib2.Request(api_vc)
 req.add_header("Accept", "application/json")
 result=urllib2.urlopen(req)
 jsonResult=json.load(result)
 
-kramerius_prefix='http://krameriusndktest.mzk.cz/search/handle/'
+kramerius_prefix='http://kramerius.mzk.cz/search/handle/'
 for item in jsonResult['response']['docs']:
-  print kramerius_prefix + item['root_pid']
+  print  kramerius_prefix + item['PID'] + "\t" +  kramerius_prefix + item['root_pid']
 
 
