cf7dc3b73c3f64eafe97e6975efe7f4927a771f1
Martin Rumanek <martin@rumanek.cz>
2015-01-22 13:25:08 +0100

    fix build

--- utils/src/main/java/cz/mzk/k4/tools/validators/ArticleValidator.java
+++ utils/src/main/java/cz/mzk/k4/tools/validators/ArticleValidator.java
@@ -1,18 +1,5 @@
 package cz.mzk.k4.tools.validators;
 
-import cz.mzk.k4.client.api.KrameriusClientApi;
-import cz.mzk.k4.tools.utils.AccessProvider;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-
-import javax.xml.namespace.NamespaceContext;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-import java.io.FileInputStream;
-import java.io.StringReader;
 
 /**
  * Created by rumanekm on 20.1.15.
