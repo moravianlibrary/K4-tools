914d65dfeb76266c815e7f665711638f4d98c5df
Jan Holman <jenyk.holman@gmail.com>
2015-06-15 13:00:51 +0200

    OCR klientská část - spring batch

---
+++ OCR/src/main/java/cz/mzk/k4/tools/ocr/BatchConfiguration.java
@@ -0,0 +1,55 @@
+package cz.mzk.k4.tools.ocr;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by holmanj on 12.6.15.
+ */
+@Configuration
+@EnableBatchProcessing
+@EnableAutoConfiguration
+@ComponentScan("cz.mzk.k4.tools")
+public class BatchConfiguration {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new Tasklet() {
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
+                        return null;
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Job job(Step step1) throws Exception {
+        return jobBuilderFactory.get("job1")
+                .incrementer(new RunIdIncrementer())
+                .start(step1)
+                .build();
+    }
+}
