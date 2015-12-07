c26281c5b95e3ac5a01fcd01fb0c717b415a8ed5
Jan Holman <jenyk.holman@gmail.com>
2015-12-07 17:35:09 +0100

    OCR - prodloužení timeoutu na 60 vteřin

--- OCR/src/main/java/cz/mzk/k4/tools/ocr/OcrApi/AbbyRestApiFactory.java
+++ OCR/src/main/java/cz/mzk/k4/tools/ocr/OcrApi/AbbyRestApiFactory.java
@@ -1,6 +1,10 @@
 package cz.mzk.k4.tools.ocr.OcrApi;
 
+import com.squareup.okhttp.OkHttpClient;
 import retrofit.RestAdapter;
+import retrofit.client.OkClient;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by holmanj on 16.6.15.
@@ -11,14 +15,20 @@ public class AbbyRestApiFactory {
 
     public static AbbyRestApi getAbbyRestApi(String abbyUrl) {
 
+        final OkHttpClient okHttpClient = new OkHttpClient();
+        okHttpClient.setReadTimeout(60, TimeUnit.SECONDS);
+        okHttpClient.setConnectTimeout(60, TimeUnit.SECONDS);
+
         RestAdapter.Builder builder = new RestAdapter.Builder()
                 .setEndpoint(PROTOCOL + abbyUrl)
+                .setClient(new OkClient(okHttpClient))
                 .setErrorHandler(new AbbyRestErrorHandler());
         AbbyRestApiJson apiJSON = builder.build().create(AbbyRestApiJson.class);
 
         builder = new RestAdapter.Builder()
                 .setConverter(new StringConverter())
                 .setEndpoint(PROTOCOL + abbyUrl)
+                .setClient(new OkClient(okHttpClient))
                 .setErrorHandler(new AbbyRestErrorHandler());
         AbbyRestApiString apiString = builder.build().create(AbbyRestApiString.class);
 
